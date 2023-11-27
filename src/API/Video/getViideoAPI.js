0edc7070ca60fb9ba84cd72561dd10e3e45f1a27
ches0703 <ches0703@naver.com>
2023-11-27 15:06:02 +0900

    [Code] emial 추가

--- src/API/Video/getViideoAPI.js
+++ src/API/Video/getViideoAPI.js
@@ -2,9 +2,9 @@ import axios from "axios"
 import baseURL from "../URL";
 const URL = baseURL + "/video/watch"
 
-export default async function getVideoAPI(videoId) {
+export default async function getVideoAPI(data) {
   const res = await axios.get(URL, {
-    params: videoId,
+    params: data,
     responseType: 'blob',
     headers: {
       'ngrok-skip-browser-warning': '69420',
