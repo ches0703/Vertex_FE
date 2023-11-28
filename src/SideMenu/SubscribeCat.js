72caf4b5b90b97f92a824a40250cb83b6334e460
tentenball <lth8905@gmail.com>
2023-11-28 18:30:31 +0900

    chore: 파일이름 수정

--- src/SideMenu/SubscribeCat.js
+++ src/SideMenu/SubscribeCat.js
@@ -10,7 +10,7 @@ import {
 } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeMain, changeSub } from "../redux/CategoryReducer"
-import { getSubscribeListAPI } from '../API/UserData/subscribeAPI';
+import { getSubscribeListAPI } from '../API/UserData/SubscribeAPI';
 
 export default function SubscribeCat() {
 
