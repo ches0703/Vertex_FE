90b7c229a4be6b50e07f71b9246bdee4a22fc2b5
tentenball <lth8905@gmail.com>
2023-11-28 17:44:49 +0900

    Feat: 구독목록 구현

--- src/API/UserData/subscribeAPI.js
+++ src/API/UserData/subscribeAPI.js
@@ -8,7 +8,7 @@ const unsubscribeURL = baseURL + '/subscription/unsubscribe';
 
 export default async function getSubscribeListAPI(email) {
   console.log(email)
-  const res = await axios.get(URL, {
+  const res = await axios.get(listURL, {
     params: email,
     headers: {
       'ngrok-skip-browser-warning': '69420',
@@ -22,10 +22,6 @@ export default async function getSubscribeListAPI(email) {
       return false;
     })
   return res;
-}
-
-const getSubScribeContentList = async () => {
-
 }
 
 const getSubScribe = async () => {
