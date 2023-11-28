89effcb59c4b1f68990b93ef624216665afa0390
hotteok00 <hojun7889@gmail.com>
2023-11-28 15:49:05 +0900

    [Merge]

--- src/API/UserData/getUserCardImg.js
+++ src/API/UserData/getUserCardImg.js
@@ -5,19 +5,23 @@ const URL = baseURL + "/user/channel/image"
 
 export default async function getUserCardImgAPI(email) {
   console.log("Get User Card API Call")
-  const res = await axios.get(URL, {
-    params: email,
-    headers: {
-      'ngrok-skip-browser-warning': '69420',
-    },
-  })
-    .then((res) => {
-      console.log(res);
-      return res
+  if (email.email) {
+    const res = await axios.get(URL, {
+      params: email,
+      headers: {
+        'ngrok-skip-browser-warning': '69420',
+      },
     })
-    .catch((e) => {
-      console.error(e)
-      return false
-    })
-  return res
+      .then((res) => {
+        console.log(res);
+        return res
+      })
+      .catch((e) => {
+        console.error(e)
+        return false
+      })
+    return res
+  } else {
+    return null
+  }
 }
