b3bb9dce44b8cc41a2d57ca8c1de174e958da0f9
tentenball <lth8905@gmail.com>
2023-11-28 21:54:23 +0900

    Fix: link error fix

--- src/API/Video/getLikeVideoListAPI.js
+++ src/API/Video/getLikeVideoListAPI.js
@@ -4,7 +4,7 @@ import baseURL from "../URL";
 const LIKELIST_URL = baseURL + "/video/like/list";
 
 export default async function getLikeVideoListAPI(email) {
-    const res = await axios.get("http://localhost:8000/video/like/list", {
+    const res = await axios.get(LIKELIST_URL, {
       params: email,
       headers: {
         'ngrok-skip-browser-warning': '69420',
